790831a021779067d77a65b471f93677b7d08c69
Subliminal Guy <subliminal_kid@posteo.de>
2021-03-26 14:22:49 +0100

    Add Datenschutz and Imprint

---
+++ src/routes/datenschutz.svelte
@@ -0,0 +1,120 @@
+<svelte:head>
+        <title>Datenschutz: Adults in the Zoom</title>
+</svelte:head>
+
+<h1>Datenschutzhinweise</h1>
+
+<h2>1. Datenschutz auf einen Blick</h2>
+<h2>Allgemeine Hinweise</h2>
+
+<p>Die folgenden Hinweise geben einen einfachen Überblick darüber, was mit Ihren personenbezogenen Daten passiert,
+ wenn Sie diese Website besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie persönlich identifiziert werden können. Ausführliche Informationen zum Thema Datenschutz entnehmen Sie unserer unter diesem Text aufgeführten Datenschutzerklärung.</p>
+<h2>Datenerfassung auf dieser Website</h2>
+
+<strong>Wer ist verantwortlich für die Datenerfassung auf dieser Website?</strong>
+
+<p>Die Datenverarbeitung auf dieser Website erfolgt durch den Websitebetreiber. Dessen Kontaktdaten können Sie dem Impressum dieser Website entnehmen.</p>
+
+<strong>Wie erfassen wir Ihre Daten?</strong>
+
+<p>Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese mitteilen. Hierbei kann es sich z. B. um Daten handeln, die Sie in ein Kontaktformular eingeben.</p>
+
+<p>Andere Daten werden automatisch oder nach Ihrer Einwilligung beim Besuch der Website durch unsere IT-Systeme erfasst. Das sind vor allem technische Daten (z. B. Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). Die Erfassung dieser Daten erfolgt automatisch, sobald Sie diese Website betreten.</p>
+
+<strong>Wofür nutzen wir Ihre Daten?</strong>
+
+<p>Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung der Website zu gewährleisten. Andere Daten können zur Analyse Ihres Nutzerverhaltens verwendet werden.</p>
+
+<strong>Welche Rechte haben Sie bezüglich Ihrer Daten?</strong>
+
+<p>Sie haben jederzeit das Recht, unentgeltlich Auskunft über Herkunft, Empfänger und Zweck Ihrer gespeicherten personenbezogenen Daten zu erhalten. Sie haben außerdem ein Recht, die Berichtigung oder Löschung dieser Daten zu verlangen. Wenn Sie eine Einwilligung zur Datenverarbeitung erteilt haben, können Sie diese Einwilligung jederzeit für die Zukunft widerrufen. Außerdem haben Sie das Recht, unter bestimmten Umständen die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Des Weiteren steht Ihnen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.</p>
+
+<p>Hierzu sowie zu weiteren Fragen zum Thema Datenschutz können Sie sich jederzeit unter der im Impressum angegebenen Adresse an uns wenden.</p>
+
+<h2>2. Hosting</h2>
+<h2>Externes Hosting</h2>
+<p>Diese Website wird bei einem externen Dienstleister gehostet (Hoster). Die personenbezogenen Daten, die auf dieser Website erfasst werden, werden auf den Servern des Hosters gespeichert. Hierbei kann es sich v. a. um IP-Adressen, Kontaktanfragen, Meta- und Kommunikationsdaten, Vertragsdaten, Kontaktdaten, Namen, Websitezugriffe und sonstige Daten, die über eine Website generiert werden, handeln.</p>
+<p>Der Einsatz des Hosters erfolgt zum Zwecke der Vertragserfüllung gegenüber unseren potenziellen und bestehenden Kunden (Art. 6 Abs. 1 lit. b DSGVO) und im Interesse einer sicheren, schnellen und effizienten Bereitstellung unseres Online-Angebots durch einen professionellen Anbieter (Art. 6 Abs. 1 lit. f DSGVO).</p>
+<p>Unser Hoster wird Ihre Daten nur insoweit verarbeiten, wie dies zur Erfüllung seiner Leistungspflichten erforderlich ist und unsere Weisungen in Bezug auf diese Daten befolgen.</p>
+
+<h2>3. Allgemeine Hinweise und Pflicht­informationen</h2>
+<h2>Datenschutz</h2>
+<p>Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.</p>
+<p>Wenn Sie diese Website benutzen, werden verschiedene personenbezogene Daten erhoben. Personenbezogene Daten sind Daten, mit denen Sie persönlich identifiziert werden können. Die vorliegende Datenschutzerklärung erläutert, welche Daten wir erheben und wofür wir sie nutzen. Sie erläutert auch, wie und zu welchem Zweck das geschieht.</p>
+<p>Wir weisen darauf hin, dass die Datenübertragung im Internet (z. B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.</p>
+<h2>Hinweis zur verantwortlichen Stelle</h2>
+<h2>Die verantwortliche Stelle für die Datenverarbeitung auf dieser Website ist:</h2>
+
+<blockquote>
+<p>
+    David Schwertgen<br>
+    Linienstr. 65<br>
+    10119 Berlin<br>
+    Tel: +49 163 3126651<br>
+    
+    eMail: subliminal_kid(at)posteo.de</p>
+
+</blockquote>
+
+
+<p>Verantwortliche Stelle ist die natürliche oder juristische Person, die allein oder gemeinsam mit anderen über die Zwecke und Mittel der Verarbeitung von personenbezogenen Daten (z. B. Namen, eMail-Adressen o. Ä.) entscheidet.</p>
+<h2>Widerruf Ihrer Einwilligung zur Datenverarbeitung</h2>
+<p>Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen Einwilligung möglich. Sie können eine bereits erteilte Einwilligung jederzeit widerrufen. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.</p>
+
+<h2>Beschwerde­recht bei der zuständigen Aufsichts­behörde</h2>
+<p>Im Falle von Verstößen gegen die DSGVO steht den Betroffenen ein Beschwerderecht bei einer Aufsichtsbehörde, insbesondere in dem Mitgliedstaat ihres gewöhnlichen Aufenthalts, ihres Arbeitsplatzes oder des Orts des mutmaßlichen Verstoßes zu. Das Beschwerderecht besteht unbeschadet anderweitiger verwaltungsrechtlicher oder gerichtlicher Rechtsbehelfe.</p>
+<h2>Recht auf Daten­übertrag­barkeit</h2>
+<p>Sie haben das Recht, Daten, die wir auf Grundlage Ihrer Einwilligung oder in Erfüllung eines Vertrags automatisiert verarbeiten, an sich oder an einen Dritten in einem gängigen, maschinenlesbaren Format aushändigen zu lassen. Sofern Sie die direkte Übertragung der Daten an einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es technisch machbar ist.</p>
+<h2>Auskunft, Löschung und Berichtigungrkeit</h2>
+<p>Sie haben im Rahmen der geltenden gesetzlichen Bestimmungen jederzeit das Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck der Datenverarbeitung und ggf. ein Recht auf Berichtigung oder Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit unter der im Impressum angegebenen Adresse an uns wenden.</p>
+<h2>Recht auf Einschränkung der Verarbeitung</h2>
+<p>Sie haben das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Hierzu können Sie sich jederzeit unter der im Impressum angegebenen Adresse an uns wenden. Das Recht auf Einschränkung der Verarbeitung besteht in folgenden Fällen:</p>
+<ul>
+    <li>Wenn Sie die Richtigkeit Ihrer bei uns gespeicherten personenbezogenen Daten bestreiten, benötigen wir in der Regel Zeit, um dies zu überprüfen. Für die Dauer der Prüfung haben Sie das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen.</li>
+    <li>Wenn die Verarbeitung Ihrer personenbezogenen Daten unrechtmäßig geschah/geschieht, können Sie statt der Löschung die Einschränkung der Datenverarbeitung verlangen.</li>
+    <li>Wenn wir Ihre personenbezogenen Daten nicht mehr benötigen, Sie sie jedoch zur Ausübung, Verteidigung oder Geltendmachung von Rechtsansprüchen benötigen, haben Sie das Recht, statt der Löschung die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen.</li>
+    <li>Wenn Sie einen Widerspruch nach Art. 21 Abs. 1 DSGVO eingelegt haben, muss eine Abwägung zwischen Ihren und unseren Interessen vorgenommen werden. Solange noch nicht feststeht, wessen Interessen überwiegen, haben Sie das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen.</li>
+
+</ul>
+
+<p>Wenn Sie die Verarbeitung Ihrer personenbezogenen Daten eingeschränkt haben, dürfen diese Daten – von ihrer Speicherung abgesehen – nur mit Ihrer Einwilligung oder zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen oder zum Schutz der Rechte einer anderen natürlichen oder juristischen Person oder aus Gründen eines wichtigen öffentlichen Interesses der Europäischen Union oder eines Mitgliedstaats verarbeitet werden.</p>
+<h2>4. Datenerfassung auf dieser Website</h2>
+<h2>Server-Log-Dateien</h2>
+<p>Der Provider der Seiten erhebt und speichert automatisch Informationen in so genannten Server-Log-Dateien, die Ihr Browser automatisch an uns übermittelt. Dies sind:</p>
+<ul>
+    <li>Browsertyp und Browserversion</li>
+    <li>verwendetes Betriebssystem</li>
+    <li>Referrer URL</li>
+    <li>Hostname des zugreifenden Rechners</li>
+    <li>Uhrzeit der Serveranfrage</li>
+    <li>IP-Adresse</li>
+</ul>
+<p>Eine Zusammenführung dieser Daten mit anderen Datenquellen wird nicht vorgenommen.</p>
+<p>Die Erfassung dieser Daten erfolgt auf Grundlage von Art. 6 Abs. 1 lit. f DSGVO. Der Websitebetreiber hat ein berechtigtes Interesse an der technisch fehlerfreien Darstellung und der Optimierung seiner Website – hierzu müssen die Server-Log-Files erfasst werden.</p>
+<h2>5. Plugins und Tools</h2>
+<h2>commento.io</h2>
+<p>Diese Seite nutzt zur Aufnahmen und Verarbeitung von Kommentaren das Plugin <a href="https://commento.io/" target="_blank">commento.io</a>.</p>
+<p>Zu diesem Zweck muss der von Ihnen verwendete Browser Verbindung zu den Servern von commento.io aufnehmen. Hierdurch erlangt der Anbieter Kenntnis darüber, dass über Ihre IP-Adresse diese Website aufgerufen wurde.</p>
+<p>Weitere Informationen zu commento.io finden Sie <a href="https://commento.io/legal">hier</a>.</p>
+<style>
+
+        
+p {
+        text-align: justify;
+        line-height: 1.8rem;
+        font-size: 1.2rem;
+}
+
+strong {
+        text-align: justify;
+        line-height: 1.8rem;
+        font-size: 1.2rem;
+}
+
+li {
+        text-align: justify;
+        line-height: 1.8rem;
+        font-size: 1.2rem;
+}
+
+</style>
---
+++ src/routes/impressum.svelte
@@ -0,0 +1,47 @@
+<svelte:head>
+        <title>Impressum: Adults in the Zoom</title>
+</svelte:head>
+
+<h1>Impressum</h1>
+<blockquote>
+<p><strong>Angaben gemäß § 5 TMG</strong><br>
+    David Schwertgen<br>
+    Linienstr. 65<br>
+    10119 Berlin<br>
+    Tel: +49 163 3126651<br>
+    
+    eMail: subliminal_kid(at)posteo.de</p>
+
+<p><strong>Verantwortlich für den Inhalt nach § 18 Abs. 2 MStV</strong><br>
+David Schwertgen<br>
+Linienstr. 65<br>
+10119 Berlin<br>
+eMail: subliminal_kid(at)posteo.de</p>
+</blockquote>
+<p>
+<strong>Haftung für Inhalte</strong></p>
+<p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+    Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+   <p><strong>Haftung für Links</strong></p>
+<p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben.
+    Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+    Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+
+<p><strong>Urheberrecht</strong></p>
+<p>
+Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten stehen unter der Lizenz <a href="https://creativecommons.org/licenses/by-nc/3.0/de/" target="_blank">CC BY-NC 3.0 DE</a>. Das Bildmaterial ist selbst erstellt. 
+Sie dürfen das Material in jedwedem Format oder Medium vervielfältigen und weiterverbreiten und das Material remixen, verändern und darauf aufbauen.</p>
+<p>Unter folgenden Bedingungen:</p>
+<p><strong>Namensnennung:</strong> Sie müssen <a href="https://creativecommons.org/licenses/by-nc/3.0/de/#">angemessene Urheber- und Rechteangaben</a> machen, einen Link zur Lizenz beifügen und angeben, ob Änderungen vorgenommen wurden. Diese Angaben dürfen in jeder angemessenen Art und Weise gemacht werden, allerdings nicht so, dass der Eindruck entsteht, der Lizenzgeber unterstütze gerade Sie oder Ihre Nutzung besonders. </p>
+<p><strong>Nicht kommerziell:</strong> Sie dürfen das Material nicht für <a href="https://creativecommons.org/licenses/by-nc/3.0/de/#">kommerzielle</a> Zwecke nutzen. </p>
+<p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+<style>
+
+        
+p {
+        text-align: justify;
+        line-height: 1.8rem;
+        font-size: 1.2rem;
+}
+
+</style>
