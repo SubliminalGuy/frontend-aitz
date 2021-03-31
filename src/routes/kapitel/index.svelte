47093cad5a1b38363104647902d379e1c2c880a1
Subliminal Guy <subliminal_kid@posteo.de>
2021-03-31 21:57:16 +0200

    Date Update

--- src/routes/kapitel/index.svelte
+++ src/routes/kapitel/index.svelte
@@ -1,33 +1,33 @@
 <script context="module">
-    import ApolloClient, { gql } from 'apollo-boost';  
-    import moment from 'moment';
+        import ApolloClient, { gql } from 'apollo-boost';  
+        import moment from 'moment';
 
-    const blogQuery = gql`
-    query Blogs {  
-            blogs {
-                    id
-                    Title
-                    Description
-                    Published
-                    Body
-                    Slug
-            }
-    }
-    `;
-    export async function preload({params, query}) {
-            const client = new ApolloClient({ 
-                    uri: 'http://bookend-aitz.herokuapp.com/graphql',
-                    fetch: this.fetch
-                     });
-            const results = await client.query({
-                    query: blogQuery
-            })
-            return {posts: results.data.blogs}
-    }
+        const blogQuery = gql`
+        query Blogs {  
+                blogs {
+                        id
+                        Title
+                        Description
+                        Published
+                        Body
+                        Slug
+                }
+        }
+        `;
+        export async function preload({params, query}) {
+                const client = new ApolloClient({ 
+                        uri: 'http://bookend-aitz.herokuapp.com/graphql',
+                        fetch: this.fetch
+                        });
+                const results = await client.query({
+                        query: blogQuery
+                })
+                return {posts: results.data.blogs}
+        }
 </script>
 
 <script>
-    export let posts;
+        export let posts;
 </script>
 
 <style>
@@ -44,7 +44,7 @@
 </style>
 
 <svelte:head>
-    <title>Kapitel: Adults in the Zoom</title>
+        <title>Kapitel: Adults in the Zoom</title>
 </svelte:head>
 
 <h1>Kapitelübersicht</h1>
@@ -52,13 +52,13 @@
 <ul>
 {#each posts as post}
 <li>
-      <a class="main-title" rel='prefetch' href='kapitel/{post.Slug}'>
+        <a class="main-title" rel='prefetch' href='kapitel/{post.Slug}'>
         {post.Title}
-      </a>
+        </a>
 </li>
 <p> 
 {moment().to(post.Published, "DD-MM-YYYY")} ago by Subliminal_Guy 
 </p>
 {/each}
 </ul>
-<h2 class="next">Das nächste Kapitel wird am 1. April 2021 veröffentlicht.</h2>
+<h2 class="next">Das nächste Kapitel wird am 8. April 2021 veröffentlicht.</h2>
