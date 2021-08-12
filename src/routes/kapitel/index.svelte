2ab04cece6d03454027e8ffac06e4b200a192f02
Subliminal Guy <subliminal_kid@posteo.de>
2021-08-12 13:04:09 +0200

    Date Change 210812

--- src/routes/kapitel/index.svelte
+++ src/routes/kapitel/index.svelte
@@ -71,7 +71,7 @@
 <h2 class="chapter-mark">5. Die Lokalgruppe</h2>
         <Chapters subchapters={fivePosts}/>
 
-<h2 class="next">Das nächste Kapitel wird am 12. August 2021 veröffentlicht.</h2>
+<h2 class="next">Das nächste Kapitel wird am 26. August 2021 veröffentlicht.</h2>
 
 
 
